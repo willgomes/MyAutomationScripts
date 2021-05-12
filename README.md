127e907f5afb560eaec2354532cb635150ab338b
Will Neto <will_lovinf@LIVE.COM>
2021-05-12 00:14:10 -0300

    Adicionando script para automação de Pull Request em multiplas branches no AzDevops

---
+++ README.md
@@ -0,0 +1,5 @@
+# Descrição
+
+Repositório para scripts de automações diversos.
+
+![](https://media.giphy.com/media/CmFMWpEa4IFtS/giphy.gif)
---
+++ psscripts/AzurePullRequestAutomation.ps1
@@ -0,0 +1,27 @@
+﻿# cd Project's path must be accessed
+
+$SourceDev = 'develop'
+$SourceHomolog = 'homolog'
+$SourceMaster = 'master'
+
+$draftHomlog = 'false';
+
+$NeedSourceDev = $false
+$devAutoComplete = 'false';
+
+$ProjectName = ''
+$RepositoryName = ''
+$WorkItems = "1".split(',') | % {iex $_}
+$Title = ''
+$Description = ''.Split(',')
+
+
+
+If($NeedSourceDev)
+{
+    az repos pr create -p $ProjectName -r $RepositoryName --work-items $WorkItems --title $Title -d $Description -t $SourceDev --draft false --auto-complete $devAutoComplete
+    $draftHomlog = 'true';
+}
+
+az repos pr create -p $ProjectName -r $RepositoryName --work-items $WorkItems --title $Title -d $Description -t $SourceHomolog --draft $draftHomlog
+az repos pr create -p $ProjectName -r $RepositoryName --work-items $WorkItems --title $Title -d $Description -t $SourceMaster --draft true
